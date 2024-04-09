2d065b23ab7f043bb360f6adc43a1b7b23794267
Joseph (Mac) Burton <macburton1000@gmail.com>
2024-04-09 12:58:13 -0300

    Temp button working based on ID

--- jQuery.js
+++ jQuery.js
@@ -1,7 +1,12 @@
 $(document).ready(function(){
 
-    
+    /*
     $('button').click(function(){
+        $('#pTest').text('test')
+    })
+    */
+
+    $('#tempID').click(function(){
         $('#pTest').text('test')
     })
 });
