8a899b5276e34efc53b1e60408740c650326bc04
Joseph (Mac) Burton <macburton1000@gmail.com>
2024-08-09 11:36:32 -0300

    Mid index reposition. Trying different method

--- templates/index_template.php
+++ templates/index_template.php
@@ -1,11 +1,29 @@
-<div class="row">
+<div id="infoContainer" class="row">
     <div id="profileContainer">
         <img id="profile" src="./Headshots/Modified Headshot V3.png" alt="Selfie">
     </div>
+    <!--
     <div id="indexContainer">
-        <p>My name is Mac Burton</p>
-        <p>I'm an Dalhousie Computer Science Undergraduate</p>
-        <p>I have a certificate in UI/UX.</p>
-        <p>I am a passionate video game, mobile=app, and web-app developer</p>
+        <p>
+            My name is Mac Burton
+            <br>
+            Computer Science Bachelors, Dalhousie
+            <br>
+            Certificate in UI/UX, Dalhousie
+            <br>
+            Video game, mobile-app, and web-app developer
+        </p>
+    </div>
+    -->
+    <div id="indexContainer">
+        <span>
+            My name is Mac Burton
+            <br>
+            Computer Science Bachelors, Dalhousie
+            <br>
+            Certificate in UI/UX, Dalhousie
+            <br>
+            Video game, mobile-app, and web-app developer
+        </span>
     </div>
 </div>
