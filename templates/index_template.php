9204e89f2b934fab7c604ce9b93a9a8b093b149c
Joseph (Mac) Burton <macburton1000@gmail.com>
2024-08-26 15:02:48 -0300

    Margin auto between Home page info. Attempting changes to center

--- templates/index_template.php
+++ templates/index_template.php
@@ -2,27 +2,30 @@
     <div id="headshotContainer" class="col-3">
         <img id="profile" src="./Headshots/Modified Headshot V3.png" alt="Selfie">
     </div>
-    <div id="indexContainer" class="col-twothirds">
-        <div class="row infoContainer">
-            <img class="bulletPointIcon" src="../Images/Bullet Point Icons/Name.png">
-            <p class="col-1"> : Mac Burton</p>
-        </div>
-        <div class="row infoContainer">
-            <img class="bulletPointIcon" src="../Images/Bullet Point Icons/Grad Cap.png">
-            <p class="col-1"> : Bachelor of Computer Science, Dalhousie</p>
-        </div>
-        <div class="row infoContainer">
-            <img class="bulletPointIcon" src="../Images/Bullet Point Icons/Certificate.png">
-            <p class="col-1"> : Certificate in UI/UX, Dalhousie</p>
-        </div>
-        <div class="row infoContainer">
-            <img class="bulletPointIcon" src="../Images/Bullet Point Icons/Suitcase.png">
-            <p class="col-1"> : Software Engineer | CAF Army Musician</p>
-        </div>
-        <div class="row infoContainer">
-            <img class="bulletPointIcon" src="../Images/Bullet Point Icons/Strength.png">
-            <p class="col-1"> : Video game, mobile-app, and web-app developer</p>
-        </div>
-    </div>    
+    <div class="col-twothirds">
+        <div id="indexContainer" >
+            <div class="row infoContainer">
+                <img class="bulletPointIcon" src="../Images/Bullet Point Icons/Name.png">
+                <p class="col-1"> : Mac Burton</p>
+            </div>
+            <div class="row infoContainer">
+                <img class="bulletPointIcon" src="../Images/Bullet Point Icons/Grad Cap.png">
+                <p class="col-1"> : Bachelor of Computer Science, Dalhousie</p>
+            </div>
+            <div class="row infoContainer">
+                <img class="bulletPointIcon" src="../Images/Bullet Point Icons/Certificate.png">
+                <p class="col-1"> : Certificate in UI/UX, Dalhousie</p>
+            </div>
+            <div class="row infoContainer">
+                <img class="bulletPointIcon" src="../Images/Bullet Point Icons/Suitcase.png">
+                <p class="col-1"> : Software Engineer | CAF Army Musician</p>
+            </div>
+            <div class="row infoContainer">
+                <img class="bulletPointIcon" src="../Images/Bullet Point Icons/Strength.png">
+                <p class="col-1"> : Video game, mobile-app, and web-app developer</p>
+            </div>
+        </div>  
+    </div> 
+      
 </div>
 <img id="cleanSign" src="./Images/Signs/S2.png">
