bd00091ab2699127065b78bf82f8058f9cbc6773
Joseph (Mac) Burton <macburton1000@gmail.com>
2024-08-26 09:23:43 -0300

    Removed invisible 'padding' on headshot. Added auto margins on home page content

--- templates/index_template.php
+++ templates/index_template.php
@@ -3,15 +3,26 @@
         <img id="profile" src="./Headshots/Modified Headshot V3.png" alt="Selfie">
     </div>
     <div id="indexContainer" class="col-twothirds">
-        <p>
-            My name is Mac Burton
-            <br>
-            Computer Science Bachelors, Dalhousie
-            <br>
-            Certificate in UI/UX, Dalhousie
-            <br>
-            Video game, mobile-app, and web-app developer
-        </p>
+        <div class="row infoContainer">
+            <img class="bulletPointIcon" src="../Images/Bullet Point Icons/Name.png">
+            <p class="col-1"> : Mac Burton</p>
+        </div>
+        <div class="row infoContainer">
+            <img class="bulletPointIcon" src="../Images/Bullet Point Icons/Grad Cap.png">
+            <p class="col-1"> : Bachelor of Computer Science, Dalhousie</p>
+        </div>
+        <div class="row infoContainer">
+            <img class="bulletPointIcon" src="../Images/Bullet Point Icons/Certificate.png">
+            <p class="col-1"> : Certificate in UI/UX, Dalhousie</p>
+        </div>
+        <div class="row infoContainer">
+            <img class="bulletPointIcon" src="../Images/Bullet Point Icons/Suitcase.png">
+            <p class="col-1"> : Software Engineer | CAF Army Musician</p>
+        </div>
+        <div class="row infoContainer">
+            <img class="bulletPointIcon" src="../Images/Bullet Point Icons/Strength.png">
+            <p class="col-1"> : Video game, mobile-app, and web-app developer</p>
+        </div>
     </div>    
 </div>
 <img id="cleanSign" src="./Images/Signs/S2.png">
